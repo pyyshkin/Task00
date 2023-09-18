91414720acf0f53daacaa68605fb6724f0145bc3
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-18 22:18:01 +0300

    Добавили задачуtask2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"{number2} является квадратом {number1}");
+}
+else
+{
+    Console.WriteLine($"{number2} не является квадратом {number1}");
+}
