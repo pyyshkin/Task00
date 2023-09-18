2c3b8a59425dd7338f2cbbc677b79b93333e8eb8
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-18 22:18:24 +0300

    Добавили задачуtask5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>0)
+{
+
+    int count = -number;
+
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count ++;
+    }
+}
+else Console.WriteLine("Некоректный ввод");
