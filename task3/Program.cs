9b70fa8b75723eeb483d2d3f0d2411f76c6236e2
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-18 22:18:10 +0300

    Добавили задачуtask3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели");
+string weekDay = Console.ReadLine();
+if (weekDay == "1")
+    Console.WriteLine($"{weekDay}-> Понедельник");
+else if (weekDay == "2")
+    Console.WriteLine($"{weekDay}-> Вторник");
+else if (weekDay == "3")
+    Console.WriteLine($"{weekDay}-> Среда");
+else if (weekDay == "4")
+    Console.WriteLine($"{weekDay}-> Четверг");
+else if (weekDay == "5")
+    Console.WriteLine($"{weekDay}-> Пятница");
+else if (weekDay == "6")
+    Console.WriteLine($"{weekDay}-> Суббота");
+else if (weekDay == "7")
+    Console.WriteLine($"{weekDay}-> Воскресенье");
+else
+    Console.WriteLine("Некорректный ввод");
