21fe0f59e620e35bfdbf90bad20fb31c148e2d2f
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-18 22:31:41 +0300

    Добавили задачуtask7

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Internal;
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последняя цифра числа: {lastDigit}");
+}
+else Console.WriteLine("Некоректный ввод");
