c8681790c794d4bc1768df4c1f5bb2dc8887dc14
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-21 21:25:42 +0300

    task11f

--- task11/Program.cs
+++ task11/Program.cs
@@ -6,8 +6,18 @@
 // 918 -> 98
 
 int number = new Random().Next(100, 1000);
-int firstDigit = number / 100;
-int lastDigit = number % 10;
-int result = firstDigit * 10 + lastDigit;
-Console.WriteLine($"{number}->{result}");
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
+// int result = firstDigit * 10 + lastDigit;
+// Console.WriteLine($"{number}->{result}");
 
+int DeleteDigit = deleteSecondDigit(number);
+Console.WriteLine($"{number}->{DeleteDigit}");
+
+int deleteSecondDigit(int num)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+}
