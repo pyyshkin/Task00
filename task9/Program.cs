7a06ee6a08e8b558026eec8558c9942736443493
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-21 20:54:44 +0300

    task9

--- task9/Program.cs
+++ task9/Program.cs
@@ -8,8 +8,21 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка [10,99]-> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа->{firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа->{secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа->{firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа->{secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа->{maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
