091d5f15545684e03285b78a008cea7a228539ac
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-21 21:15:32 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int result = firstDigit * 10 + lastDigit;
+Console.WriteLine($"{number}->{result}");
+
